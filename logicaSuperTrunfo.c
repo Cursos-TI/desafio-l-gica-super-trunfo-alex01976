e900b4d4175aa02fe6cff51805eb1e852fb2443c
alex01976 <alexgcmnit@gmail.com>
2025-04-19 19:12:26 +0000

    Entrando com printf

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-// Siga os comentários para implementar cada parte do desafio.
-
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+printf ("Desafio Super-Trunfo 2 - Novato.\n");
+
+    // 
 
     
     // Cadastro das Cartas:
